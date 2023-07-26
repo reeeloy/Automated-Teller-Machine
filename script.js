f012b48b84697097c575334579057b72ff34097d
Alejandro Gómez <jequinsil1200@gmail.com>
2023-07-26 14:31:59 -0500

    reconstruccion del ATM

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
-let cuentas = [
-    { nombre: "Mali", saldo: 200, password: "6545"}, // 0
-    { nombre: "Gera", saldo: 290, password: "ee89"}, // 1
-    { nombre: "Maui", saldo: 67, password: "91pt" } // 2
-];
 
-document.querySelector("#persona").addEventListener("change",leerPersona)
+    var cuentas = [
+      { nombre: "Mali", saldo: 200 },
+      { nombre: "Gera", saldo: 290 },
+      { nombre: "Maui", saldo: 67 }
+    ];
 
+    var selectedAccount;
+    var passwordInput = document.getElementById("password");
+    var opcionesDiv = document.getElementById("opciones");
+    var resultadoDiv = document.getElementById("resultado");
 
+    function ingresar() {
+      var selectedOption = document.getElementById("cuentasSelect").value;
+      var password = passwordInput.value;
 
-
-// cuentas[1] <- Gera
-
-function leerPersona(){
-    let indice = parseInt(document.querySelector("#persona").value);
-    // console.log(cuentas[indice]);
-    let nombre = cuentas[indice].nombre;
-    let saldo = cuentas[indice].saldo;
-
-    if(saldo < 100){
-        alert("Tu saldo se esta acabando");
+      if (cuentas[selectedOption].password === password) {
+        selectedAccount = cuentas[selectedOption];
+        passwordInput.value = "";
+        opcionesDiv.style.display = "block";
+      } else {
+        alert("Contraseña incorrecta. Intenta nuevamente.");
+        passwordInput.value = "";
+        opcionesDiv.style.display = "none";
+      }
     }
 
-    document.getElementById("saludo").innerHTML = "Hola, " + nombre;
+    function consultarSaldo() {
+      resultadoDiv.innerHTML = "Saldo actual: $" + selectedAccount.saldo;
+    }
 
-    document.getElementById("meter-password").style.display = "block"
+    function ingresarMonto() {
+      var monto = prompt("Ingrese el monto a ingresar:");
+      monto = parseInt(monto);
 
-}
+      if (!isNaN(monto)) {
+        if (monto <= 0) {
+          alert("El monto a ingresar debe ser mayor a cero.");
+        } else {
+          selectedAccount.saldo += monto;
+          resultadoDiv.innerHTML = "Monto ingresado: $" + monto + "<br> Nuevo saldo: $" + selectedAccount.saldo;
+        }
+      } else {
+        alert("Ingresa un monto válido.");
+      }
+    }
 
-function irAotraPagina(){
-    window.location.href = "exito.html"
-}
+    function retirarMonto() {
+      var monto = prompt("Ingrese el monto a retirar:");
+      monto = parseInt(monto);
 
-// leerPersona();
-
+      if (!isNaN(monto)) {
+        if (monto <= 0) {
+          alert("El monto a retirar debe ser mayor a cero.");
+        } else if (monto > selectedAccount.saldo) {
+          alert("No tienes suficiente saldo para retirar esa cantidad.");
+        } else if (selectedAccount.saldo - monto < 10) {
+          alert("No puedes retirar esa cantidad, debe haber al menos $10 en la cuenta.");
+        } else {
+          selectedAccount.saldo -= monto;
+          resultadoDiv.innerHTML = "Monto retirado: $" + monto + "<br> Nuevo saldo: $" + selectedAccount.saldo;
+        }
+      } else {
+        alert("Ingresa un monto válido.");
+      }
+    }
